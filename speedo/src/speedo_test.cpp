636054262b4f606d5d9f03237d916da374c30802
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-03-31 10:29:05 +0200

    Add subroutine to example program.

--- speedo/src/speedo_test.cpp
+++ speedo/src/speedo_test.cpp
@@ -1,6 +1,14 @@
-#include "speedo/speedo.h"
 #define PROFILE 1
+#include "speedo/speedo.h"
 
+
+// Example function.
+void function()
+{
+___ for (int x = 0; x < 1e9; x++)
+        ;
+___
+}
 
 // Example program that shows how to use speedo.
 int main()
@@ -11,6 +19,8 @@ ___    for (long int i = 0; i < 10000000; ++i)
 ___
     for (long int i = 0; i < 10000000; ++i)
         ;
+
+    function();
 ___
     return 0;
 }
