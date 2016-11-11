f916663ec3da7c84902bb14e4c9a85d38cbd95e9
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2016-11-11 13:53:23 +0100

    Make example code more sensible.

--- speedo/src/speedo_test.cpp
+++ speedo/src/speedo_test.cpp
@@ -3,26 +3,29 @@
 
 
 // Example function.
-void function()
+double function()
 {
-___ for (int x = 0; x < 1e9; x++)
-        double c = 1.2e34 / 9.8e76;
-___
+    double c = 1.2e34;
+xxx for (int x = 0; x < 1e9; ++x)
+        c /= 1.1;
+xxx
+    return c;
 }
 
 
 // Example program that shows how to use speedo.
 int main()
 {
-___ for (int j = 0; j < 20; j++)
+    double a = 1.2345;
+xxx for (int j = 0; j < 20; j++)
         for (long int i = 0; i < 100000; ++i)
-___         double a = 1.2345 * 9.8765;
-    
-for (long int i = 0; i < 100000; ++i)
-        double b = 1.2345 / 9.8765;
-___
+xxx         a *= 9.8765;
+
+    for (long int i = 0; i < 100000; ++i)
+        a *= 9.8765;
+xxx
 
     function();
-___
+xxx
     return EXIT_SUCCESS;
 }
